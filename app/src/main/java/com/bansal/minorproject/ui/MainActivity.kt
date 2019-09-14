22d1b67951b1901673a703791c918f8d2716cd7c
Nihit Bansal <nihitb06.dev@gmail.com>
2019-09-14 14:19:04 +0530

    Added Fonts

---
+++ app/src/main/java/com/bansal/minorproject/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bansal.minorproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bansal.minorproject.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
