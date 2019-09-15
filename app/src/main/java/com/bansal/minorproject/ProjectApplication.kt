d82aa6c41320fc312ae05d555b7cf16569469ab7
Nihit Bansal <nihitb06.dev@gmail.com>
2019-09-15 16:23:43 +0530

    Added a DayNight Mode for Application

---
+++ app/src/main/java/com/bansal/minorproject/ProjectApplication.kt
@@ -0,0 +1,13 @@
+package com.bansal.minorproject
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class ProjectApplication : Application () {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
